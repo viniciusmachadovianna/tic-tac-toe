da90845120daf7bbad7a7e4ec86916ce85523b84
viniciusmachadovianna <viniciusmachadovianna@outlook.com>
2024-12-24 13:50:43 -0300

    noscript acessibility

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-const $=(el)=>document.querySelector(el);
-const header = document.querySelector("header"),
+const $=(el)=>document.querySelector(el),
+    id=(el)=>document.getElementById(el),
+    header = document.querySelector("header"),
+    card = document.querySelector('nav'),
+    line = document.querySelector('rect'),
     main = document.querySelector("main"),
-    line = document.querySelector('rect');
+    tiles = main.querySelectorAll('div'),
+    footer = document.querySelector("footer");
 var turn = "✖", //◯
-    turnsTaken = 0,
-    winsO = 0,
-    winsX = 0,
-    reset=false,
-    freeze=false;
+    turnsTaken = 0;
+header.style.display='flex';
+main.style.display='none';
+card.style.display='none';
+footer.style.display='none';
 header.addEventListener("click",()=>{
     header.style.display = "none";
+    card.style.display = "flex";
     main.style.display = "flex";
+    footer.style.display = "flex";
 });
+card.addEventListener('click',()=>{
+    card.style.transform='rotateX(180deg)';
+})
+function changeBG(el, color = 'none'){
+    el.style.background=color;
+}
 function resetBoard(){
-    main.querySelectorAll("DIV").forEach((tile)=>{
+    tiles.forEach((tile)=>{
         tile.replaceChildren();
-        tile.style.background='purple';
+        changeBG(tile,'purple');
+        tile.style.color='aliceblue';
     })
-    reset=true;
     line.style.display='none';
     switchTurn();
 }
 function switchTurn(){
     turn = turn === "✖" ? "◯" : "✖";
 }
-document.querySelectorAll("div").forEach(div =>{
-    div.addEventListener('click',(e)=>{
-        if(div.style.background !== "none"){
-            div.innerHTML = turn;
-            turnsTaken += 1;
-            if(turnsTaken>=5){checkVictoryCondition()};
-            div.style.background = "none";
-            switchTurn();
-        }
+tiles.forEach(tile =>{
+    tile.addEventListener('click',()=>{
+        if(tile.style.background === "none"){return}
+        tile.innerHTML = turn;
+        turnsTaken += 1;
+        if(turnsTaken>=5){checkVictoryCondition()};
+        changeBG(tile);
+        switchTurn();
     })
     
-    div.addEventListener('mouseover',()=>{
-        if(div.style.background !== "none" && !reset && !freeze){div.innerHTML = turn;}
+    tile.addEventListener('mouseover',()=>{
+        if(tile.style.background !== "none"){tile.innerHTML = turn;}
     })
     
-    div.addEventListener('mouseout',()=>{
-        if(div.style.background !== "none"){div.textContent = "";}
+    tile.addEventListener('mouseout',()=>{
+        if(tile.style.background !== "none"){tile.textContent = "";}
     })
 });
 function checkVictoryCondition(){
@@ -53,16 +64,20 @@ function checkVictoryCondition(){
         {cells:["a3","b3","c3"],deg:90,y:-110},
         {cells:["a1","b2","c3"],deg:45,y:0},
         {cells:["a3","b2","c1"],deg:-45,y:0} 
-    ];
+       ];
 
     combinations.forEach((condition) => {
         if (condition.cells.every(id => document.getElementById(id).innerHTML === turn)) {
-            freeze=true;
+            tiles.forEach((tile)=>{
+                changeBG(tile);
+            })
+            condition.cells.forEach(tileId => {
+                id(tileId).style.color = 'rgb(93, 255, 174)';
+            });
             line.style.display='flex';
             line.style.transform=`rotate(${condition.deg}deg) translateY(${condition.y}px)`
             const score = document.getElementById(turn);
             score.innerHTML = parseInt(score.innerHTML) + 1;
-            // resetBoard();
         }
     })
 }
