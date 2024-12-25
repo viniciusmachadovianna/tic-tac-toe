b94dd63fad1b0b2595ae5b08879439c0fa39d2ab
viniciusmachadovianna <viniciusmachadovianna@outlook.com>
2024-12-25 12:30:37 -0300

    bg behavior js check
    css changed from colored to transparent, inverting the js checks logic

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ const $=(el)=>document.querySelector(el),
 var turn = "✖", //◯
     turnsTaken = 0,
     winsX=0,
-    winsO=0;
-header.style.display='flex';
-main.style.display='none';
-card.style.display='none';
-card.textContent=turn;
+    winsO=0,
+    gameFrozen=false;
 footer.style.display='none';
+function changeBG(el,color=null){
+    el.style.background=color;
+}
 header.addEventListener("click",()=>{
     header.style.display = "none";
     card.style.display = "flex";
@@ -30,7 +30,7 @@ card.addEventListener('mouseout',()=>{
 })
 restart.addEventListener('mouseover',()=>{
     restart.style.transform='rotate(180deg)';
-    restart.style.background='rgb(185, 4, 4)';
+    changeBG(restart,'rgb(185, 4, 4)');
     restart.querySelector('img').style.filter='invert()';
 })
 restart.addEventListener('mouseout',()=>{
@@ -38,16 +38,14 @@ restart.addEventListener('mouseout',()=>{
     restart.style.background='aliceblue';
     restart.querySelector('img').style.filter='invert(0)';
 })
-function changeBG(el, color = 'none'){
-    el.style.background=color;
-}
 function resetBoard(){
     turnsTaken=0;
+    gameFrozen=false;
     restart.style.background='aliceblue';
     restart.querySelector('img').style.filter='invert(0)';
     tiles.forEach((tile)=>{
         tile.textContent='';
-        changeBG(tile,'rgba(67, 0, 67,.9)');
+        changeBG(tile);
         tile.style.color='aliceblue';
     })
     line.style.display='none';
@@ -62,14 +60,14 @@ function switchTurn(){
 }
 tiles.forEach(tile =>{
     tile.addEventListener('click',()=>{
-        if(tile.style.background === "none"){return}
-        tile.innerHTML = turn;
+        if(!tile.style.background||gameFrozen){return}
+        tile.textContent = turn;
         turnsTaken += 1;
         if(turnsTaken>=5){checkVictoryCondition()};
         if(turnsTaken===9){
             tiles.forEach((tile)=>{
                 tile.style.color='lightgray';
-                restart.style.background='rgb(185, 4, 4)';
+                changeBG(restart,'rgb(185, 4, 4)');
                 restart.querySelector('img').style.filter='invert(1)';
             })
         }
@@ -78,34 +76,31 @@ tiles.forEach(tile =>{
     })
     
     tile.addEventListener('mouseover',()=>{
-        if(tile.style.background==="none"){return}
+        if(tile.textContent||gameFrozen){return}
         tile.innerHTML = turn;
-        tile.style.background='purple';
+        changeBG(tile,'rgba(67, 0, 67,.3');
     })
     
     tile.addEventListener('mouseout',()=>{
-        if(tile.style.background==="none"){return}
-        tile.textContent = "";
-        tile.style.background='rgba(67, 0, 67,.9)';
+        if(!tile.style.background||gameFrozen){return}
+        tile.textContent=null;
+        changeBG(tile);
     })
 });
 function checkVictoryCondition(){
     const combinations = [
-        {cells:["a1","a2","a3"],deg:0,  y:50,   x:0},
-        {cells:["b1","b2","b3"],deg:0,  y:160,  x:0},
-        {cells:["c1","c2","c3"],deg:0,  y:270,  x:0},
-        {cells:["a1","b1","c1"],deg:90, y:110,  x:160},
-        {cells:["a2","b2","c2"],deg:90, y:0,    x:160},
-        {cells:["a3","b3","c3"],deg:90, y:-110, x:160},
-        {cells:["a1","b2","c3"],deg:45, y:112.5,x:112.5},
-        {cells:["a3","b2","c1"],deg:-45,y:112.5,x:-112.5} 
+        {cells:["a1","a2","a3"],deg:0,  y:45,   x:0},
+        {cells:["b1","b2","b3"],deg:0,  y:150,  x:0},
+        {cells:["c1","c2","c3"],deg:0,  y:255,  x:0},
+        {cells:["a1","b1","c1"],deg:90, y:105,  x:150},
+        {cells:["a2","b2","c2"],deg:90, y:0,    x:150},
+        {cells:["a3","b3","c3"],deg:90, y:-105, x:150},
+        {cells:["a1","b2","c3"],deg:45, y:105,x:105},
+        {cells:["a3","b2","c1"],deg:-45,y:105,x:-105}
     ];
     
     combinations.forEach((condition) => {
         if (condition.cells.every(id => document.getElementById(id).innerHTML === turn)) {
-            tiles.forEach((tile)=>{
-                changeBG(tile);
-            })
             condition.cells.forEach(tileId => {
                 id(tileId).style.color = 'rgb(93, 255, 174)';
                 changeBG(id(turn),'rgb(93, 255, 174)');
@@ -128,6 +123,7 @@ function checkVictoryCondition(){
             else if(winsX>winsO){x.className='fav';}
             else{o.className='fav'}
             turnsTaken=0;
+            gameFrozen=true;
         }
     })
 }
